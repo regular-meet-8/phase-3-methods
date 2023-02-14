e8ea7da7c322e985c646b574747066c879562583
regular-meet-8 <eddiekim444@gmail.com>
2023-02-14 09:13:36 +0300

    methods

--- methods.rb
+++ methods.rb
@@ -1,1 +1,25 @@
 # Your code here!
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+def greet(name = "zizu")
+    puts "Hello, #{name}!"
+end 
+
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+    return num1 + num2 
+end 
+
+def halve(num)
+    if num === num.to_i 
+        return num / 2 
+    else 
+        puts "try again"
+    end
+end
